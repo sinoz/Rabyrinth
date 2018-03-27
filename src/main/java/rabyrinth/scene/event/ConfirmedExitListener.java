fae82b8920c027a8382f553604dc2b56692a1cf8
Sino <ilyas.alfan.b@gmail.com>
2018-03-27 17:57:41 +0200

    Add exit delay

--- src/main/java/rabyrinth/scene/event/ConfirmedExitListener.java
+++ src/main/java/rabyrinth/scene/event/ConfirmedExitListener.java
@@ -1,12 +1,21 @@
 package rabyrinth.scene.event;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.google.common.eventbus.Subscribe;
+
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.*;
 
 /** @author Sino */
 public final class ConfirmedExitListener {
+	private final Stage stage;
+
+	public ConfirmedExitListener(Stage stage) {
+		this.stage = stage;
+	}
+
 	@Subscribe
 	public void confirmedExit(ConfirmedExit event) {
-		Gdx.app.exit();
+		stage.addAction(sequence(delay(2.0F), run(() -> Gdx.app.exit())));
 	}
 }
